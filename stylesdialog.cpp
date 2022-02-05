9d5a23aff3ce98ad08ab2ede053d1bfd3e567e63
bcat <v01z@mail.ru>
2022-02-05 18:11:05 +0300

    cat asks for meat

--- stylesdialog.cpp
+++ stylesdialog.cpp
@@ -7,6 +7,16 @@ StylesDialog::StylesDialog(QWidget *parent) :
     ui(new Ui::StylesDialog)
 {
     ui->setupUi(this);
+
+    const QString styles [] = {
+        { tr("Morning") },
+        { tr("Day") },
+        { tr("Evening") },
+        { tr("Evening") },
+        { tr("Night") }
+
+    };
+
 }
 
 StylesDialog::~StylesDialog()
@@ -16,33 +26,71 @@ StylesDialog::~StylesDialog()
 
 void StylesDialog::on_btnClose_clicked()
 {
-    //qDebug() << ui->groupBoxStyle->isChecked();
 
-//    QList<QRadioButton *> allRadioButtons = groupBox.findChildren<QRadioButton *>();
     QList<QRadioButton*> styleButtons = ui->groupBoxStyle->findChildren<QRadioButton*>();
     qDebug () << styleButtons.size();
 
-    const auto checkedRadBtn {std::find_if(styleButtons.begin(), styleButtons.end(),
+    const auto checkedRadBtn { std::find_if(styleButtons.begin(), styleButtons.end(),
         [](const auto &radioButton){ return radioButton->isChecked(); }) };
 
-    //qDebug() << *checkedRadBtn->text();
     qDebug() << (*checkedRadBtn)->text();
 
-    /*
-    for (const auto &elem: styleButtons)
+    if ((*checkedRadBtn)->text() == "Summer")
     {
-        if (elem->isChecked())
-        {
-            qDebug()<< elem->text() << " is checked.";
-            if (elem->text() == "Summer")
-            {
-                qApp->setStyleSheet(
-                    "QPushButton  {font: bold 14 px; background-color: red;}" \
-                        "QMainWindow{background-color:grey}");
-            }
-            break;
-        }
     }
-    */
+    else
+    {
+        QPalette lightPalette;
+        lightPalette.setColor(QPalette::Window, QColor(11,11,11));
+        qApp->setPalette(lightPalette);
+    }
+
    close();
+}
+
+void StylesDialog::on_rbLighter_clicked()
+{
+    ui->rbEvening->setEnabled(false);
+    ui->rbNight->setEnabled(false);
+
+    ui->rbMorning->setEnabled(true);
+    ui->rbDay->setEnabled(true);
+}
+
+
+void StylesDialog::on_rbDarker_clicked()
+{
+    ui->rbEvening->setEnabled(true);
+    ui->rbNight->setEnabled(true);
+
+    ui->rbMorning->setEnabled(false);
+    ui->rbDay->setEnabled(false);
+
+}
+
+void StylesDialog::setAppStyle(const QString &style)
+{
+        QPalette palette;
+
+        palette.setColor(QPalette::Window, darkGray);
+        palette.setColor(QPalette::WindowText, Qt::white);
+        palette.setColor(QPalette::Base, black);
+        palette.setColor(QPalette::AlternateBase, darkGray);
+        palette.setColor(QPalette::ToolTipBase, blue);
+        palette.setColor(QPalette::ToolTipText, Qt::white);
+        palette.setColor(QPalette::Text, Qt::white);
+        palette.setColor(QPalette::Button, darkGray);
+        palette.setColor(QPalette::ButtonText, Qt::white);
+        palette.setColor(QPalette::Link, blue);
+        palette.setColor(QPalette::Highlight, blue);
+        palette.setColor(QPalette::HighlightedText, Qt::black);
+
+        palette.setColor(QPalette::Active, QPalette::Button, gray.darker());
+        palette.setColor(QPalette::Disabled, QPalette::ButtonText, gray);
+        palette.setColor(QPalette::Disabled, QPalette::WindowText, gray);
+        palette.setColor(QPalette::Disabled, QPalette::Text, gray);
+        palette.setColor(QPalette::Disabled, QPalette::Light, darkGray);
+
+        qApp->setPalette(palette);
+
 }
--- stylesdialog.h
+++ stylesdialog.h
@@ -15,11 +15,23 @@ public:
     explicit StylesDialog(QWidget *parent = nullptr);
     ~StylesDialog();
 
+    void setAppStyle(const QString &);
+
 private slots:
     void on_btnClose_clicked();
+
+    void on_rbLighter_clicked();
+
+    void on_rbDarker_clicked();
 
 private:
     Ui::StylesDialog *ui;
 };
+
+   //colors
+   const QColor darkGray(53, 53, 53);
+   const QColor gray(128, 128, 128);
+   const QColor black(25, 25, 25);
+   const QColor blue(42, 130, 218);
 
 #endif // STYLESDIALOG_H
